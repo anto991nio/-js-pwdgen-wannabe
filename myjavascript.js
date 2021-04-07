4cefb92a6971e63073d564839f194cb6cdb29b44
anto991nio <antonio.91.cardone@gmail.com>
2021-04-07 16:01:22 +0200

    Termina esercizio

---
+++ myjavascript.js
@@ -0,0 +1,8 @@
+var nome = prompt("Qual è il tuo nome?");
+var cognome = prompt("Qual è il tuo cognome?");
+var colorepreferito = prompt("Qual è il tuo colore preferito?");
+var anni = prompt("Quanti anni hai?");
+document.getElementById("myname").innerHTML = "il tuo nome è "+ "" + nome ;
+document.getElementById("mysurname").innerHTML = "il tuo cognome è "+ "" + cognome ;
+document.getElementById("mycolor").innerHTML = "il tuo colore preferito è "+ "" + colorepreferito ;
+document.getElementById("myage").innerHTML = "la tua età è "+ "" + anni ;
